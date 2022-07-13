bf4526178f63b3a79582a5c98219d9cb6d7db5f6
keanaido021@ <keanaido021@student.wethinkcode.co.za>
2022-07-13 18:58:48 +0200

    new commit

---
+++ test/dart_language_samples_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_language_samples/dart_language_samples.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
